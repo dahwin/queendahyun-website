4eabca9ab08239b86a24c6f572d4dcd95de947a3
dahwin <116961692+dahwin@users.noreply.github.com>
2024-07-13 02:09:45 +0600

    Update SignupPage_page.tsx

--- src/SignupPage_page.tsx
+++ src/SignupPage_page.tsx
@@ -4,7 +4,7 @@ import axios from 'axios';
 import { countries } from 'countries-list';
 import Cookies from 'js-cookie';
 
-const API_BASE_URL = 'http://127.0.0.1:8000/api';
+const API_BASE_URL = 'http://localhost:8000/api';
 
 interface SignupPageProps {
   setIsAuthenticated: (value: boolean) => void;
