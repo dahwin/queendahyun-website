d11cf872c2eb932ddc88d8aa734480abcb45408c
dahwin <116961692+dahwin@users.noreply.github.com>
2024-07-13 19:14:00 +0600

    done

--- src/SignupPage_page.tsx
+++ src/SignupPage_page.tsx
@@ -4,7 +4,7 @@ import axios from 'axios';
 import { countries } from 'countries-list';
 import Cookies from 'js-cookie';
 
-const API_BASE_URL = 'http://localhost:8000/api';
+const API_BASE_URL = 'https://www.queendahyun.com/api';
 
 
 interface SignupPageProps {
--- src/user_page.tsx
+++ src/user_page.tsx
@@ -15,7 +15,7 @@ interface UserPageProps {
   onLogout: () => void;
 }
 
-const BASE_URL = 'http://localhost:8000/api';
+const BASE_URL = 'https://www.queendahyun.com/api';
 
 const UserPage: React.FC<UserPageProps> = ({ onLogout }) => {
   const [userData, setUserData] = useState<UserData | null>(null);
